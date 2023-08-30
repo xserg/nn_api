ace10695337dcb887bab121bd2f7ed30ddf0bc7e
xserg <xxserg@gmail.com>
2023-08-30 14:20:36 +0300

    middleware update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,6 +68,5 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'abilities' => \Laravel\Sanctum\Http\Middleware\CheckAbilities::class,
         'ability' => \Laravel\Sanctum\Http\Middleware\CheckForAnyAbility::class,
-        'request.logging' => \App\Http\Middleware\ApiRequestLogging::class,
     ];
 }
